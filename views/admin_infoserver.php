9c828d5b6d8ee7182066648d76faf1cb3a124ca2
Anthony <darknoon29@users.noreply.github.com>
2014-11-03 16:45:07 +0100

    Supression des statistiques serveur

--- views/admin_infoserver.php
+++ views/admin_infoserver.php
@@ -4,7 +4,7 @@
 *	desc.		:
 *	Author		: Kyser - http://ogsteam.fr/
 *	created		: 16/12/2005
-*	modified	: 02/11/2011 02:45:00
+*	modified	: 04/11/2014 02:45:00
 ***************************************************************************/
 
 if (!defined('IN_SPYOGAME')) {
@@ -48,41 +48,17 @@ $nb_users = 0;
 $og_uni = 'unknow';
 $og_pays = 'unknow';
 
-// recuperation du pays et de l univers du serveur
-if (isset($server_config["xtense_universe"])) {
-    //pattern de recherche
-    $pattern = "#http://uni([0-9]{1,3}+)\.ogame\.([\w]{2,3})#";
-    if (preg_match($pattern, $server_config["xtense_universe"], $retour)) {
-        $og_pays = $retour[2]; // seconde capture
-        $og_uni = $retour[1]; // premiere capture
-    }
-}
-
-if (defined("OGSPY_KEY")) {
-    if (check_var($serveur_key, 'Text')) {
-        $key = $serveur_key;
-    }
-
-    $paths = 'http://' . $_SERVER["SERVER_NAME"];
-    if (check_var($serveur_date, 'Num')) {
-        $since = $serveur_date;
-    }
-} else {
-    echo 'Fichier key.php introuvable !!! ', log_("key");
-}
-
 $request = "select statistic_name, statistic_value from " . TABLE_STATISTIC;
 $result = $db->sql_query($request);
 
 while (list($statistic_name, $statistic_value) = $db->sql_fetch_row($result)) {
 
 	switch ($statistic_name) {
+        case "connection_server":
+        	$connection_server = $statistic_value;
+        	break;
 
-		case "connection_server":
-			$connection_server = $statistic_value;
-			break;
-
-		case "planetimport_ogs":
+	case "planetimport_ogs":
             $planetimport_ogs = $statistic_value;
             break;
 
@@ -116,127 +92,6 @@ list($connectes) = $db->sql_fetch_row($connectes_req);
 //Personne en ligne
 $online = session_whois_online();
 
-
-//Vérification version installée et envoi de statistiques
-preg_match("#([0-9]+)\.([0-9]+)\.([0-9]+)(\-[a-z]*)?#", $server_config["version"],
-    $current_version);
-@list($current_version, $head_revision, $minor_revision, $extension_revision) =
-    $current_version;
-
-$errno = 0;
-$errstr = $version_info = '';
-
-//proxy patch by Cerber
-//based on http://us2.php.net/manual/en/function.fsockopen.php#58196
-//vrai si on doit passer par un proxy, faux sinon
-$proxy_use = false;
-//Nom du serveur de proxy
-$proxy_name = 'proxy';
-//port utilisé par le serveur proxy
-$proxy_port = 3128;
-//utilisateur du proxy
-$proxy_user = '';
-//mot de passe de l'utilisateur
-$proxy_pass = '';
-
-//Adresse du serveur a contacter
-$url_server = "update.ogsteam.fr";
-//port du serveur spécifié en hard dans le code
-
-$fsock = false;
-if ($proxy_use) {
-    //on utlise le proxy
-    $fsock = @fsockopen($proxy_name, $proxy_port, $errno, $errstr, 3);
-} else {
-    //pas de proxy
-    $fsock = @fsockopen($url_server, 80, $errno, $errstr, 3);
-}
-
-if ($fsock) {
-    //paramètres de la requete
-    $link = "/ogspy/latest2.php";
-    $link .= "?version=" . $server_config["version"];
-
-    $link .= "&nb_users=" . $users_info;
-
-    $link .= "&connection_server=" . $connection_server;
-
-    $link .= "&planetimport_ogs=" . $planetimport_ogs;
-    $link .= "&planetexport_ogs=" . $planetexport_ogs;
-
-    $link .= "&spyimport_ogs=" . $spyimport_ogs;
-    $link .= "&spyexport_ogs=" . $spyexport_ogs;
-
-    $link .= "&rankimport_ogs=" . $rankimport_ogs;
-    $link .= "&rankexport_ogs=" . $rankexport_ogs;
-
-    // clef unique
-    $link .= "&server_paths=" . $paths;
-    $link .= "&server_since=" . $since;
-    $link .= "&server_key=" . $key;
-
-    // recuperation pays et univers du serveur
-    $link .= "&og_uni=" . $og_uni;
-    $link .= "&og_pays=" . $og_pays;
-
-    if ($proxy_use) {
-        //si on passe par le proxy ==> requête sauce proxy
-
-        //création de l'url réellement recherchée
-        $request_url = "http://$url_server:80$link";
-        //appel de l'url via le proxy
-        @fputs($fsock, "GET $request_url HTTP/1.0\r\nHost: $proxy_name\r\n");
-        if (isset($proxy_user) && $proxy_user != '') {
-            //ajout du login + pass si dispo
-            @fputs($fsock, "Proxy-Authorization: Basic " . base64_encode("$proxy_user:$proxy_pass") .
-                "\r\n");
-        }
-        //demande de cloture de connexion
-        @fputs($fsock, "Connection: close\r\n\r\n");
-    } else {
-        //pas de proxy ==> code standard
-        @fputs($fsock, "GET " . $link . " HTTP/1.1\r\n");
-        @fputs($fsock, "HOST: " . $url_server . "\r\n");
-        @fputs($fsock, "Connection: close\r\n\r\n");
-    }
-
-    $get_info = false;
-    while (!@feof($fsock)) {
-        if ($get_info) {
-            $version_info .= @fread($fsock, 1024);
-        } else {
-            if (@fgets($fsock, 1024) == "\r\n") {
-                $get_info = true;
-            }
-        }
-    }
-
-    @fclose($fsock);
-    if (preg_match("#([0-9]+)\.([0-9]+)\.([0-9]+)(\-[a-z]*){0,1}#", $version_info, $version_info)) {
-
-        @list($latest_version, $latest_head_revision, $latest_minor_revision, $latest_extension_revision) =
-            $version_info;
-        $version_info = $latest_version;
-
-        if (version_compare($latest_head_revision . '.' . $latest_minor_revision . '.' .
-            $latest_extension_revision, $head_revision . '.' . $minor_revision . '.' . $extension_revision,
-            '<=')) {
-            $version_info = "<font color='lime'><b>Votre serveur OGSpy est à jour.</b></font>";
-            /*$version_info .='Latest_Head: '.$latest_head_revision.' Minor: '.$latest_minor_revision.' Ext: '.$latest_extension_revision.' vs Head:'.$head_revision.' Minor: '.$minor_revision.' Ext: '.$extension_revision;*/
-        } else {
-            $version_info = "<blink><b><font color='red'>Votre serveur OGSpy n'est pas à jour.</font></blink>";
-            $version_info .= "<br />Rendez vous sur le  <a href='http://www.ogsteam.fr' target='_blank'>forum</a> dédié au support d'OGSpy pour récupérer la dernière version : <font color='red'>" .
-                $latest_version . "</b>";
-        }
-    } else {
-        $version_info = "<blink><b><font color='orange'>Une incohérence a été rencontrée avec le serveur de contrôle de version.</font></blink>";
-        $version_info .= "<br />Consulter le <a href='http://www.ogsteam.fr' target='_blank'>forum</a> dédié au support d'OGSpy pour en connaître la raison.</b>";
-    }
-} else {
-    $version_info = "<blink><b><font color='orange'>Impossible de récupérer le numéro de la dernière version car le lien n'a pas pu être établie avec le serveur de contrôle.</font></blink>";
-    $version_info .= "<br />Il se peut que ce soit votre hébergeur qui n'autorise pas cette action.";
-    $version_info .= "<br />Il vous faudra consulter régulièrement le <a href='http://board.ogsteam.fr' target='_blank'>forum</a> dédié au support d'OGSpy pour prendre connaissance des nouvelles versions.</b>";
-}
 ?>
 
 <table width="100%">
@@ -280,10 +135,6 @@ help("drop_sessions"); ?>)</th>
 	<th><a>Classement (nombre de lignes)</a></th>
 	<th><?php echo formate_number($rankimport_ogs); ?> importations - <?php echo formate_number($rankexport_ogs); ?> exportations</th>
 </tr>
-<!--<tr>
-	<th><a>Rapports de combats [Serveur]</a></th><th>x importations</th>
-	<th><a>Rapports de combats [OGS]</a></th><th>x importations - x exportations</th>
-</tr>-->
 <tr>
 	<td class="c" colspan="4">&nbsp;</th>
 </tr>
