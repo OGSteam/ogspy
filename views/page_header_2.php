55ffb94b405e9812537dd0522c81ee27ae610817
Allegre <Allegre@Allegre-PC.www.huaweimobilewifi.com>
2015-03-17 21:27:02 +0100

    Mise en forme XHTML page login correcte + bas de page (HTML)

--- views/page_header_2.php
+++ views/page_header_2.php
@@ -19,26 +19,22 @@ if ($link_css == "") {
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
 		<meta name="language" content="fr" />
 		<title><?php echo $server_config["servername"]." - OGSpy ".$server_config["version"];?></title>
 		
 		<link rel="stylesheet" type="text/css" href="<?php echo $link_css;?>formate.css" />
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-		<link rel="icon" href="favicon.ico" type="image/icon" />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<link rel="icon" type="image/icon" href="favicon.ico" />
 	</head>
 	
 	<body>
-		<table width="100%" align="center" cellpadding="20">
-			<tr>
-				<td height="70">
-					<div align="center">
-						<img src="<?php echo $server_config["default_skin"].$banner_selected;?>" />
-					</div>
-				</td>
-			</tr>
+		<table style="text-align:center; width:100%; padding:20px;">
+			<tr><td>
+                <img style="margin-bottom:30px;" alt="Logo OGSpy" src="<?php echo $server_config["default_skin"].$banner_selected;?>" />
+            </td></tr>
 			
 			<tr>
 				<td>
