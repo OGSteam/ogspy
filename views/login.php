071c76254c9f8824897d9113ec1db7fe722d5d2d
Allegre <pitch314@users.noreply.github.com>
2015-03-17 21:27:02 +0100

    Mise en forme XHTML page login correcte + bas de page (HTML)

--- views/login.php
+++ views/login.php
@@ -22,42 +22,39 @@ $enable_register_view = isset ($server_config['enable_register_view']) ? $server
 
 ?>
 
-<form method='post' action=''>
-	<input type='hidden' name='action' value='login_web' />
-	<input type='hidden' name='goto' value='<?php echo $goto;?>' />
+<form style="margin-bottom:40px;" method='post' action=''>
+	<p><input type='hidden' name='action' value='login_web' />
+	<input type='hidden' name='goto' value='<?php echo $goto;?>' /></p>
 	
-	<table align='center' cellpadding="0" cellspacing="1">
+	<table style="margin:0 auto; padding:0; border-collapse:separate; border-spacing:1px">
 		<tr>
-			<td class="c" colspan="2">Paramètres de connexion</td>
+			<td class="c" colspan="2" style="text-align:left">Paramètres de connexion</td>
 		</tr>
 		<tr>
-			<th width="150">Login :</th>
-			<th width="150"><input type='text' name='login' /></th>
+			<th style="width:150px">Login :</th>
+			<th style="width:150px"><input type='text' name='login' /></th>
 		</tr>
 		<tr>
-			<th>Mot de passe :</th>
-			<th><input type='password' name='password' /></th>
+			<th style="width:150px">Mot de passe :</th>
+			<th style="width:150px"><input type='password' name='password' /></th>
 		</tr>
 		<tr>
-			<th colspan='2' align='right'><input type='submit' value='Connexion' /></th>
+			<th colspan='2'><input type='submit' value='Connexion' /></th>
 		</tr>
-		
 		<?php
 		
 		if ($enable_register_view == 1 ) {
 		
 		?>
-		
 		<tr>
-			<td class="c" colspan="2">Demande de compte OGSpy</td>
+			<td class="c" colspan="2" style="text-align:left">Demande de compte OGSpy</td>
 		</tr>
 		<tr>
-			<th colspan='2' align='right'>Si vous ne disposez pas d'un compte, il faut <font color='red'>obligatoirement</font> en demander un sur le forum de <?php echo $server_config['register_alliance']; ?>.</th>
+			<th colspan='2'>Si vous ne disposez pas d'un compte, il faut <span style="color:red">obligatoirement</span> en demander un sur le forum de <?php echo $server_config['register_alliance']; ?>.</th>
 		</tr>
 		<tr>
-			<th colspan='2' align='right'><input type="button" value="Demander un compte" onclick="window.open('<?php echo $server_config['register_forum']; ?>');" /></th>
+			<th colspan='2'><input type="button" value="Demander un compte" onclick="window.open('<?php echo $server_config['register_forum']; ?>');" /></th>
 		</tr>
-		
 		<?php
 		
 		}
@@ -66,4 +63,9 @@ $enable_register_view = isset ($server_config['enable_register_view']) ? $server
 	</table>
 </form>
 
+<p style="text-align:right;">
+ <a href="http://validator.w3.org/check?uri=referer">
+  <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+ </a>
+</p>
 <?php require_once("views/page_tail_2.php"); ?>
