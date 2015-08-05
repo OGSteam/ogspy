3089fb55da16988e20dc0205aa95aaf457231fbd
darknoon29 <darknoon@darkcity.fr>
2015-08-05 14:39:14 +0200

    Gestion de la langue dans l'installation

--- lang/lang_main.php
+++ lang/lang_main.php
@@ -3,7 +3,12 @@
 //ui lang à récupérer dans parameters.php
 $ui_lang="french";
 
-require_once ("lang/".$ui_lang."/lang_install.php");
+if (defined("INSTALL_IN_PROGRESS") || defined("UPGRADE_IN_PROGRESS")) {
+	require_once ("../lang/".$ui_lang."/lang_install.php");
+}else
+{
+	require_once ("./lang/".$ui_lang."/lang_install.php");
+}
 
 
 
