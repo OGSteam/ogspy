3370b2612a08ff3254c36ef755ca7e121abe1144
Anthony <darknoon29@users.noreply.github.com>
2015-11-07 00:11:00 +0100

    Traduction Page admin Affichage

--- lang/french/lang_admin.php
+++ lang/french/lang_admin.php
@@ -5,3 +5,46 @@
  * Date: 05/08/2015
  * Time: 18:15
  */
+
+/* admin.php */
+$lang['ADMIN_TITLE_GENERAL_INFO'] = "Informations générales";
+$lang['ADMIN_TITLE_SERVER_CONF'] = "Paramètres du serveur";
+$lang['ADMIN_TITLE_DISPLAY_CONF'] = "Paramètres d'affichage";
+$lang['ADMIN_TITLE_MEMBER_CONF'] = "Gestion des membres";
+$lang['ADMIN_TITLE_GROUP_CONF'] = "Gestion des groupes";
+$lang['ADMIN_TITLE_LOGS_CONF'] = "Journal";
+$lang['ADMIN_TITLE_MODS_CONF'] = "Modules";
+
+/* admin_affichage.php */
+$lang['ADMIN_DISPLAY_GALAXY_TITLE'] = "Option de la page: Galaxie";
+$lang['ADMIN_DISPLAY_GALAXY_MIPS'] = "Affichage des MIPs";
+$lang['ADMIN_DISPLAY_STATS_TITLE'] = "Option de la page: Statistique";
+$lang['ADMIN_DISPLAY_STATS_MEMBER'] = "Affichage des statistiques membres";
+$lang['ADMIN_DISPLAY_STATS_CONNECTED'] = "Affichage des membres connectés";
+$lang['ADMIN_DISPLAY_STATS_GVIEW'] = "Nombre de galaxies par ligne <a>[1-100]</a>";
+$lang['ADMIN_DISPLAY_STATS_SVIEW'] = "Nombre de systèmes par ligne <a>[1-100]</a>";
+$lang['ADMIN_DISPLAY_ALLY_TITLE'] = "Option de la page: Espace alliance";
+$lang['ADMIN_DISPLAY_ALLY_COLUMS'] = "Nombre de colones <a>[1-9]</a>";
+$lang['ADMIN_DISPLAY_ALLY_COLOR'] = "Couleur de l'alliance";
+$lang['ADMIN_DISPLAY_ALLY_COLORDESC'] = "Doit être le nom d'une couleur en anglais ou son code précédé d'un #";
+$lang['ADMIN_DISPLAY_ALLY_GVIEW'] = "Nombre de galaxies par ligne <a>[1-100]</a>";
+$lang['ADMIN_DISPLAY_ALLY_SVIEW'] = "Nombre de systèmes par ligne <a>[1-100]</a>";
+$lang['ADMIN_DISPLAY_LOGIN_TITLE'] = "Option de la connexion";
+$lang['ADMIN_DISPLAY_LOGIN_REGISTER'] = "Affichage du panneau inscription";
+$lang['ADMIN_DISPLAY_LOGIN_ALLYNAME'] = "Nom de l'alliance";
+$lang['ADMIN_DISPLAY_LOGIN_FORUM'] = "Lien du forum d'inscription";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE'] = "Module de connexion utilisateur";
+$lang['ADMIN_DISPLAY_LOGIN_ADMINMODULE'] = "Module de connexion admins";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_ADMIN'] = "Administration";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_PROFILE'] = "Profil";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_ACCOUNT'] = "Espace personnel";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_GALAXY'] = "Galaxie";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_ALLY'] = "Espace alliance";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_SEARCH'] = "Recherche";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_RANKINGS'] = "Classement";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_STATS'] = "Statistiques";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_TOBEUPDATED'] = "Syst. obsolètes";
+
+$lang['ADMIN_DISPLAY_SUBMIT'] = "Valider";
+$lang['ADMIN_DISPLAY_RESET'] = "Réinitialiser";
+
