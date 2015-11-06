8bb07aff1f6c98e59be9c5e4a22e198fd9991cfe
darknoon29 <darknoon@darkcity.fr>
2015-11-07 00:22:54 +0100

    Conversion UTF8

--- lang/french/lang_admin.php
+++ lang/french/lang_admin.php
@@ -7,9 +7,9 @@
  */
 
 /* admin.php */
-$lang['ADMIN_TITLE_GENERAL_INFO'] = "Informations générales";
-$lang['ADMIN_TITLE_SERVER_CONF'] = "Paramètres du serveur";
-$lang['ADMIN_TITLE_DISPLAY_CONF'] = "Paramètres d'affichage";
+$lang['ADMIN_TITLE_GENERAL_INFO'] = "Informations gÃ©nÃ©rales";
+$lang['ADMIN_TITLE_SERVER_CONF'] = "ParamÃ¨tres du serveur";
+$lang['ADMIN_TITLE_DISPLAY_CONF'] = "ParamÃ¨tres d'affichage";
 $lang['ADMIN_TITLE_MEMBER_CONF'] = "Gestion des membres";
 $lang['ADMIN_TITLE_GROUP_CONF'] = "Gestion des groupes";
 $lang['ADMIN_TITLE_LOGS_CONF'] = "Journal";
@@ -20,15 +20,15 @@ $lang['ADMIN_DISPLAY_GALAXY_TITLE'] = "Option de la page: Galaxie";
 $lang['ADMIN_DISPLAY_GALAXY_MIPS'] = "Affichage des MIPs";
 $lang['ADMIN_DISPLAY_STATS_TITLE'] = "Option de la page: Statistique";
 $lang['ADMIN_DISPLAY_STATS_MEMBER'] = "Affichage des statistiques membres";
-$lang['ADMIN_DISPLAY_STATS_CONNECTED'] = "Affichage des membres connectés";
+$lang['ADMIN_DISPLAY_STATS_CONNECTED'] = "Affichage des membres connectÃ©s";
 $lang['ADMIN_DISPLAY_STATS_GVIEW'] = "Nombre de galaxies par ligne <a>[1-100]</a>";
-$lang['ADMIN_DISPLAY_STATS_SVIEW'] = "Nombre de systèmes par ligne <a>[1-100]</a>";
+$lang['ADMIN_DISPLAY_STATS_SVIEW'] = "Nombre de systÃ¨mes par ligne <a>[1-100]</a>";
 $lang['ADMIN_DISPLAY_ALLY_TITLE'] = "Option de la page: Espace alliance";
 $lang['ADMIN_DISPLAY_ALLY_COLUMS'] = "Nombre de colones <a>[1-9]</a>";
 $lang['ADMIN_DISPLAY_ALLY_COLOR'] = "Couleur de l'alliance";
-$lang['ADMIN_DISPLAY_ALLY_COLORDESC'] = "Doit être le nom d'une couleur en anglais ou son code précédé d'un #";
+$lang['ADMIN_DISPLAY_ALLY_COLORDESC'] = "Doit Ãªtre le nom d'une couleur en anglais ou son code prÃ©cÃ©dÃ© d'un #";
 $lang['ADMIN_DISPLAY_ALLY_GVIEW'] = "Nombre de galaxies par ligne <a>[1-100]</a>";
-$lang['ADMIN_DISPLAY_ALLY_SVIEW'] = "Nombre de systèmes par ligne <a>[1-100]</a>";
+$lang['ADMIN_DISPLAY_ALLY_SVIEW'] = "Nombre de systÃ¨mes par ligne <a>[1-100]</a>";
 $lang['ADMIN_DISPLAY_LOGIN_TITLE'] = "Option de la connexion";
 $lang['ADMIN_DISPLAY_LOGIN_REGISTER'] = "Affichage du panneau inscription";
 $lang['ADMIN_DISPLAY_LOGIN_ALLYNAME'] = "Nom de l'alliance";
@@ -43,8 +43,8 @@ $lang['ADMIN_DISPLAY_LOGIN_MODULE_ALLY'] = "Espace alliance";
 $lang['ADMIN_DISPLAY_LOGIN_MODULE_SEARCH'] = "Recherche";
 $lang['ADMIN_DISPLAY_LOGIN_MODULE_RANKINGS'] = "Classement";
 $lang['ADMIN_DISPLAY_LOGIN_MODULE_STATS'] = "Statistiques";
-$lang['ADMIN_DISPLAY_LOGIN_MODULE_TOBEUPDATED'] = "Syst. obsolètes";
+$lang['ADMIN_DISPLAY_LOGIN_MODULE_TOBEUPDATED'] = "Syst. obsolÃ¨tes";
 
 $lang['ADMIN_DISPLAY_SUBMIT'] = "Valider";
-$lang['ADMIN_DISPLAY_RESET'] = "Réinitialiser";
+$lang['ADMIN_DISPLAY_RESET'] = "RÃ©initialiser";
 
