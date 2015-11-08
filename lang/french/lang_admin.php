892c8dfe93da1c6778271764e3ccc4ea1a5728d3
darknoon29 <darknoon@darkcity.fr>
2015-11-08 16:02:01 +0100

    Traduction Page admin infoserver

--- lang/french/lang_admin.php
+++ lang/french/lang_admin.php
@@ -6,6 +6,11 @@
  * Time: 18:15
  */
 
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
 /* admin.php */
 $lang['ADMIN_TITLE_GENERAL_INFO'] = "Informations générales";
 $lang['ADMIN_TITLE_SERVER_CONF'] = "Paramètres du serveur";
@@ -48,3 +53,30 @@ $lang['ADMIN_DISPLAY_LOGIN_MODULE_TOBEUPDATED'] = "Syst. obsolètes";
 $lang['ADMIN_DISPLAY_SUBMIT'] = "Valider";
 $lang['ADMIN_DISPLAY_RESET'] = "Réinitialiser";
 
+/* admin_infoserver.php*/
+
+$lang['ADMIN_SERVER_STATS'] = "Statistiques";
+$lang['ADMIN_SERVER_STATS_VALUE'] = "Valeur";
+$lang['ADMIN_SERVER_MEMBERS'] = "Nombre de membres";
+$lang['ADMIN_SERVER_FREEPLANETS'] = "Nombre de planètes répertoriées libres";
+$lang['ADMIN_SERVER_NBPLANETS'] = "Nombre de planètes répertoriées";
+$lang['ADMIN_SERVER_DB_SIZE'] = "Espace occupé par la base de données";
+$lang['ADMIN_SERVER_LOG_SIZE'] = "Espace occupé par les logs";
+$lang['ADMIN_SERVER_DB_OPTIMIZE'] = "Optimiser la base de données";
+$lang['ADMIN_SERVER_SESSIONS'] = "Nombre de session ouvertes";
+$lang['ADMIN_SERVER_SESSIONS_CLEAN'] = "vider";
+$lang['ADMIN_SERVER_CONNEXIONS'] = "Connexions au serveur";
+$lang['ADMIN_SERVER_PLANETS'] = "Planètes";
+$lang['ADMIN_SERVER_ALL_IMPORT'] = "importations";
+$lang['ADMIN_SERVER_ALL_EXPORT'] = "exportations";
+$lang['ADMIN_SERVER_SPYREPORTS'] = "Rapports d'espionnage";
+$lang['ADMIN_SERVER_RANKINGS'] = "Classement (nombre de lignes)";
+$lang['ADMIN_SERVER_PHPINFO'] = "PHPInfo";
+$lang['ADMIN_SERVER_PHPMODULES'] = "Modules PHP";
+$lang['ADMIN_SERVER_INFOVERSION'] = "Information de version";
+$lang['ADMIN_SERVER_INFOVERSION_DESC'] = "Merci de consulter le Forum de l'OGSteam pour connaitre la dernière version d'OGSpy: <a href='http://www.ogsteam.fr' target='_blank'>Forum</a>";
+
+$lang['ADMIN_SERVER_MEMBERNAME'] = "Nom du membre";
+$lang['ADMIN_SERVER_MEMBERCONNECTED'] = "Connexion";
+$lang['ADMIN_SERVER_MEMBERLASTACTIVITY'] = "Dernière activité";
+$lang['ADMIN_SERVER_MEMBERIP'] = "Adresse IP";
--- views/admin_infoserver.php
+++ views/admin_infoserver.php
@@ -96,84 +96,83 @@ $online = session_whois_online();
 
 <table width="100%">
     <tr>
-        <td class="c_stats" width="25%">Statistiques</td>
-        <td class="c" width="25%">Valeur</td>
-        <td class="c_stats" width="25%">Statistiques</td>
-        <td class="c" width="25%">Valeur</td>
+        <td class="c_stats" width="25%"><?php echo($lang['ADMIN_SERVER_STATS']); ?></td>
+        <td class="c" width="25%"><?php echo($lang['ADMIN_SERVER_STATS_VALUE']); ?></td>
+        <td class="c_stats" width="25%"><?php echo($lang['ADMIN_SERVER_STATS']); ?></td>
+        <td class="c" width="25%"><?php echo($lang['ADMIN_SERVER_STATS_VALUE']); ?></td>
     </tr>
     <tr>
-        <th><a>Nombre de membres</a></th>
+        <th><a><?php echo($lang['ADMIN_SERVER_MEMBERS']); ?></a></th>
         <th><?php echo $users_info; ?></th>
-        <th><a>Nombre de planètes répertoriées libres</a></th>
+        <th><a><?php echo($lang['ADMIN_SERVER_FREEPLANETS']); ?></a></th>
         <th><?php echo
             formate_number($galaxy_statistic["nb_planets_free"]); ?></th>
     </tr>
     <tr>
-        <th><a>Nombre de planètes répertoriées</a></th>
+        <th><a><?php echo($lang['ADMIN_SERVER_NBPLANETS']); ?></a></th>
         <th><?php echo formate_number($galaxy_statistic["nb_planets"]); ?></th>
-        <th rowspan="2"><a>Espace occupé par la base de données</a></th>
+        <th rowspan="2"><a><?php echo($lang['ADMIN_SERVER_DB_SIZE']); ?></a></th>
         <th><?php echo
             $db_size_info; ?></th>
     </tr>
     <tr>
-        <th><a>Espace occupé par les logs</a></th>
+        <th><a><?php echo($lang['ADMIN_SERVER_LOG_SIZE']); ?></a></th>
         <th><?php echo $log_size_info; ?></th>
-        <th><a href="index.php?action=db_optimize"><i>Optimiser la base de données</i></a></th>
+        <th><a href="index.php?action=db_optimize"><i><?php echo($lang['ADMIN_SERVER_DB_OPTIMIZE']); ?></i></a></th>
     </tr>
     <tr>
-        <th><a>Nombre de session ouvertes</a></th>
-        <th><?php echo $connectes; ?><a href="index.php?action=drop_sessions"> (vider <?php echo
+        <th><a><?php echo($lang['ADMIN_SERVER_SESSIONS']); ?></a></th>
+        <th><?php echo $connectes; ?><a href="index.php?action=drop_sessions"> (<?php echo($lang['ADMIN_SERVER_SESSIONS_CLEAN']); ?> <?php echo
                 help("drop_sessions"); ?>)</th>
-        <th colspan='2'>&nbsp;</th>
+        <th colspan='2'></th>
     </tr>
     <tr>
-        <th colspan='4'>&nbsp;</th>
+        <th colspan='4'></th>
     </tr>
     <tr>
-        <th><a>Connexions au serveur</a></th>
+        <th><a><?php echo($lang['ADMIN_SERVER_CONNEXIONS']); ?></a></th>
         <th><?php echo formate_number($connection_server); ?></th>
 
-        <th><a>Planètes</a></th>
-        <th><?php echo formate_number($planetimport_ogs); ?> importations
-            - <?php echo formate_number($planetexport_ogs); ?> exportations
+        <th><a><?php echo($lang['ADMIN_SERVER_PLANETS']); ?></a></th>
+        <th><?php echo formate_number($planetimport_ogs); ?> <?php echo($lang['ADMIN_SERVER_ALL_IMPORT']); ?>
+            - <?php echo formate_number($planetexport_ogs); ?> <?php echo($lang['ADMIN_SERVER_ALL_EXPORT']); ?>
         </th>
     </tr>
     <tr>
-        <th><a>Rapports d'espionnage</a></th>
-        <th><?php echo formate_number($spyimport_ogs); ?> importations - <?php echo formate_number($spyexport_ogs); ?>
-            exportations
+        <th><a><?php echo($lang['ADMIN_SERVER_SPYREPORTS']); ?></a></th>
+        <th><?php echo formate_number($spyimport_ogs); ?> <?php echo($lang['ADMIN_SERVER_ALL_IMPORT']); ?> - <?php echo formate_number($spyexport_ogs); ?>
+            <?php echo($lang['ADMIN_SERVER_ALL_EXPORT']); ?>
         </th>
 
-        <th><a>Classement (nombre de lignes)</a></th>
-        <th><?php echo formate_number($rankimport_ogs); ?> importations - <?php echo formate_number($rankexport_ogs); ?>
-            exportations
+        <th><a><?php echo($lang['ADMIN_SERVER_RANKINGS']); ?></a></th>
+        <th><?php echo formate_number($rankimport_ogs); ?> <?php echo($lang['ADMIN_SERVER_ALL_IMPORT']); ?> - <?php echo formate_number($rankexport_ogs); ?>
+            <?php echo($lang['ADMIN_SERVER_ALL_EXPORT']); ?>
         </th>
     </tr>
     <tr>
         <td class="c" colspan="4">&nbsp;</td>
     </tr>
     <tr>
-        <th colspan="2"><a href="php/phpinfo.php" target="_blank">PHPInfo</a></th>
-        <th colspan="2"><a href="php/phpmodules.php" target="_blank">Modules PHP</a></th>
+        <th colspan="2"><a href="php/phpinfo.php" target="_blank"><?php echo($lang['ADMIN_SERVER_PHPINFO']); ?></a></th>
+        <th colspan="2"><a href="php/phpmodules.php" target="_blank"><?php echo($lang['ADMIN_SERVER_PHPMODULES']); ?></a></th>
     </tr>
 </table>
 <br/>
 <table width="100%">
     <tr>
-        <td class="c_ogspy">Information de version</td>
+        <td class="c_ogspy"><?php echo($lang['ADMIN_SERVER_INFOVERSION']); ?></td>
     </tr>
     <tr>
-        <th style="text-align:left">Merci de consulter le Forum de l'OGSteam pour connaitre la dernière version d'OGSpy:
-            <a href='http://www.ogsteam.fr' target='_blank'>Forum</a></th>
+        <th style="text-align:left"><?php echo($lang['ADMIN_SERVER_INFOVERSION_DESC']); ?></th>
     </tr>
 </table>
 <br/>
 <table width="100%">
     <tr>
-        <td class="c_user">Nom de membre</td>
-        <td class="c">Connexion</td>
-        <td class="c">Dernière activité</td>
-        <td class="c_tech">Adresse IP</td>
+        <td class="c_user"><?php echo($lang['ADMIN_SERVER_MEMBERNAME']); ?></td>
+        <td class="c"><?php echo($lang['ADMIN_SERVER_MEMBERCONNECTED']); ?></td>
+        <td class="c"><?php echo($lang['ADMIN_SERVER_MEMBERLASTACTIVITY']); ?></td>
+        <td class="c_tech"><?php echo($lang['ADMIN_SERVER_MEMBERIP']); ?></td>
     </tr>
     <?php
     foreach ($online as $v) {
