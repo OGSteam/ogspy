35cbceb45a53d9ad6badf04398dffbe310d0b90f
Anthony <darknoon29@users.noreply.github.com>
2016-01-06 17:56:06 +0100

    Galaxie Strings

--- lang/french/lang_galaxy.php
+++ lang/french/lang_galaxy.php
@@ -5,3 +5,67 @@
  * Date: 05/08/2015
  * Time: 18:16
  */
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
+/* galaxy.php */
+
+$lang['GALAXY_SELECT_GALAXY'] = "Galaxie";
+$lang['GALAXY_SELECT_SYSTEM'] = "Système solaire";
+$lang['GALAXY_DISPLAY'] = "Afficher";
+$lang['GALAXY_FAVORITE_LIST'] = "Liste des systèmes favoris";
+$lang['GALAXY_MAX_FAVORITES_MESSAGE'] = "Vous avez atteint le nombre maximal de favoris permis";
+$lang['GALAXY_NO_FAVORITES_MESSAGE'] = "Vous n\'avez pas de favoris";
+$lang['GALAXY_ADD_FAVORITES'] = "Ajouter aux favoris";
+$lang['GALAXY_REMOVE_FAVORITES'] = "Supprimer des favoris";
+$lang['GALAXY_SYSTEMS'] = "Système solaire ";
+$lang['GALAXY_PLANETS'] = "Planètes";
+$lang['GALAXY_ALLIES'] = "Alliances";
+$lang['GALAXY_PLAYERS'] = "Joueurs";
+$lang['GALAXY_UPDATES'] = "Mises à jour";
+$lang['GALAXY_ALLY'] = "Alliance ";
+$lang['GALAXY_PLAYER'] = "Joueur ";
+$lang['GALAXY_RANK'] = "Classement du ";
+$lang['GALAXY_RANK_GENERAL'] = "Général";
+$lang['GALAXY_RANK_ECONOMY'] = "Economique";
+$lang['GALAXY_RANK_LAB'] = "Recherche";
+$lang['GALAXY_RANK_MILITARY'] = "Militaire";
+$lang['GALAXY_RANK_MILITARY_BUILT'] = "Militaire construit";
+$lang['GALAXY_RANK_MILITARY_LOST'] = "Perte militaire";
+$lang['GALAXY_RANK_MILITARY_DESTROYED'] = "Destruction";
+$lang['GALAXY_RANK_MILITARY_HONNOR'] = "Honneur";
+$lang['GALAXY_MEMBERS'] = "membre(s)";
+$lang['GALAXY_SEE_DETAILS'] = "Voir détails";
+$lang['GALAXY_CR'] = "RC";
+$lang['GALAXY_SR'] = "RE";
+$lang['GALAXY_LEGEND'] = "Légende";
+$lang['GALAXY_INACTIVE_7Days'] = "Inactif 7 jours";
+$lang['GALAXY_INACTIVE_7Days_SYMBOL'] = "i";
+$lang['GALAXY_INACTIVE_28Days'] = "Inactif 28 jours";
+$lang['GALAXY_INACTIVE_28Days_SYMBOL'] = "I";
+$lang['GALAXY_HOLIDAYS'] = "Mode vacances";
+$lang['GALAXY_HOLIDAYS_SYMBOL'] = "v";
+$lang['GALAXY_WEAK_PROTECTION'] = "Joueur faible";
+$lang['GALAXY_WEAK_PROTECTION_SYMBOL'] = "d";
+$lang['GALAXY_MOON'] = "Lune";
+$lang['GALAXY_MOON_PHALANX'] = "phalange 4 avec porte spatial";
+$lang['GALAXY_MOON_PHALANX_SYMBOL'] = " - 4P";
+$lang['GALAXY_SPYREPORT'] = "Rapport d\'espionnage";
+$lang['GALAXY_SPYREPORT_SYMBOL'] = "RE";
+$lang['GALAXY_COMBATREPORT'] = "Rapports de combat";
+$lang['GALAXY_COMBATREPORT_SYMBOL'] = "X RC";
+$lang['GALAXY_ALLY_FRIEND'] = "Joueur / Alliance allié";
+$lang['GALAXY_ALLY_HIDDEN'] = "Joueur / Alliance masqué";
+$lang['GALAXY_PHALANX_LIST'] = "Liste des phalanges hostiles dans le secteur";
+$lang['GALAXY_LUNA_GATE'] = "porte spatiale";
+$lang['GALAXY_NO_PHALANX'] = "Aucune phalange répertoriée n'a une portée suffisante pour phalanger les planètes de ce système";
+$lang['GALAXY_SEARCH'] = "Recherches";
+$lang['GALAXY_SEARCH_PLANETS_AVAILABLE'] = "Planètes colonisables";
+$lang['GALAXY_SEARCH_MOONS'] = "Lunes";
+$lang['GALAXY_SEARCH_INACTIVES'] = "Joueurs inactifs";
+$lang['GALAXY_SEARCH_SPYREPORTS'] = "Rapports d'espionnage";
+
+
+
