cbed268184373b35675b7004871c1eca16dac6d8
darknoon29 <darknoon@darkcity.fr>
2016-02-03 20:27:55 +0100

    Nommage des langues suivant norme iso

---
+++ lang/fr/lang_cartography.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antho
+ * Date: 05/08/2015
+ * Time: 18:16
+ */
+
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
+/* cartography.php */
+
+$lang['CARTO_ALLY'] = "Alliance ";
+$lang['CARTO_DISPLAYPOSITIONS'] = "Afficher les positions";
+$lang['CARTO_PLAYER_POSITIONS'] = "Joueur(s) présent(s)";
--- lang/lang_main.php
+++ lang/lang_main.php
@@ -1,7 +1,7 @@
 <?php
 
 //ui lang à récupérer dans parameters.php
-$ui_lang="french";
+$ui_lang="fr";
 
 if (defined("INSTALL_IN_PROGRESS") || defined("UPGRADE_IN_PROGRESS")) {
 	require_once ("../lang/".$ui_lang."/lang_install.php");
