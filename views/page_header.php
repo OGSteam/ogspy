0aee0001db83ee0cef981cc8c4d3c30c36eb0cf4
Anthony <darknoon29@users.noreply.github.com>
2016-02-19 18:50:55 +0100

    D?but travaux

--- views/page_header.php
+++ views/page_header.php
@@ -18,13 +18,15 @@ if (!defined('IN_SPYOGAME')) {
     <meta name="language" content="fr">
     <title><?php echo $server_config["servername"] . " - OGSpy " . $server_config["version"];?></title>
     <link rel="stylesheet" type="text/css" href="./skin/OGSpy_skin/formate.css"/>
+    <link rel="stylesheet" href="js/jquery-ui/jquery-ui.css" >
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <link rel="icon" type="image/icon" href="favicon.ico">
     <link rel="alternate" type="application/rss+xml" title="Flux RSS OGSpy" href="rss.xml"/>
 
     <!-- Ajout des scripts Graphique (highcharts), jquery et prototype-->
 
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-ui/jquery-ui.js"></script>
     <script type="text/javascript" src="js/highcharts.js"></script>
 </head>
 <body>
