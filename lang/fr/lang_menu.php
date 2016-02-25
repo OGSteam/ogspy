fa95e902e6ab1c6b5403e2f24809579cae6115fb
Anthony <darknoon29@users.noreply.github.com>
2016-02-25 19:06:02 +0100

    Traduction Menu

--- lang/fr/lang_menu.php
+++ lang/fr/lang_menu.php
@@ -16,3 +16,17 @@
 $lang['MENU_SERVER_TIME'] = "Heure serveur";
 $lang['MENU_WAITING'] = "En attente";
 $lang['MENU_SERVER_OFFLINE'] = "Serveur hors-ligne";
+$lang['MENU_ADMIN'] = "Administration";
+$lang['MENU_PROFILE'] = "Profil";
+$lang['MENU_HOME'] = "Espace Personnel";
+$lang['MENU_GALAXY'] = "Galaxie";
+$lang['MENU_ALLIANCES'] = "Alliances";
+$lang['MENU_RESEARCH'] = "Recherche";
+$lang['MENU_RANKINGS'] = "Classements";
+$lang['MENU_UPDATE_STATUS'] = "Etat Cartographie";
+$lang['MENU_MODULES'] = "Modules";
+$lang['MENU_FORUM'] = "Forum Alliance";
+$lang['MENU_ABOUT'] = "A Propos";
+$lang['MENU_LOGOUT'] = "Déconnexion";
+
+
