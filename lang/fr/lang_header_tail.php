b04496ae61edaffb65288fc894bdf668273157b3
darknoon29 <darknoon@darkcity.fr>
2016-02-29 23:11:32 +0100

    Header Tails Traduction

--- lang/fr/lang_header_tail.php
+++ lang/fr/lang_header_tail.php
@@ -5,3 +5,23 @@
  * Date: 05/08/2015
  * Time: 18:18
  */
+
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
+/* page_header.php */
+
+$lang['HEAD_LANGUAGE'] = "fr";
+$lang['HEAD_RSS_STREAM'] = "Flux RSS OGSpy";
+
+/* page_tail.php */
+
+$lang['FOOTER_PHPERRORS'] = "Erreurs php";
+$lang['FOOTER_RENDERING'] = "Temps de génération";
+$lang['FOOTER_OGSPY'] = "est un outil";
+$lang['FOOTER_CONTRIBUTORS'] = "contributeur";
+$lang['FOOTER_ON_SITE'] = "sur le site";
+$lang['FOOTER_QUERY'] = "requête";
+
