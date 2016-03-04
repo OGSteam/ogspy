e6b57b8731509308d08fa529a3ba11ab59ac5a66
Anthony <darknoon29@users.noreply.github.com>
2016-03-04 23:18:11 +0100

    Trad Page statistiques : End

--- lang/fr/lang_statistic.php
+++ lang/fr/lang_statistic.php
@@ -5,3 +5,26 @@
  * Date: 05/08/2015
  * Time: 18:22
  */
+
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
+/* statistic.php */
+
+$lang['STATS_TITLE'] = "État de la cartographie";
+$lang['STATS_LEGEND'] = "Légende";
+$lang['STATS_KNOWN_PLANETS'] = "Planètes répertoriées";
+$lang['STATS_FREE_PLANETS'] = "Planètes colonisables";
+$lang['STATS_UPDATED_PLANETS'] = "Planètes mises à jour récemment";
+$lang['STATS_RAZ'] = "Remise à zéro";
+$lang['STATS_USERNAME'] = "Pseudos";
+$lang['STATS_PLANETS'] = "Planètes";
+$lang['STATS_SPY_REPORTS'] = "Rapports d'espionnage";
+$lang['STATS_RANKINGS'] = "Classement (lignes)";
+$lang['STATS_SEARCHINGS'] = "Recherches<br/>effectuées";
+$lang['STATS_RATIO'] = "Ratio";
+$lang['STATS_XTENSE'] = "Xtense";
+$lang['STATS_CONNECTED'] = "connecté sur le serveur";
+$lang['STATS_CONNECTED_XTENSE'] = "connecté avec Xtense ou Xtense Chrome Plugin";
