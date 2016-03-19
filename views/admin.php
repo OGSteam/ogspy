66b6fe45109e23fa7e939471862f51e0a9b70c89
Anthony <darknoon29@users.noreply.github.com>
2016-03-19 13:13:10 +0100

    Removing <font> (obsolete) from HTML code

--- views/admin.php
+++ views/admin.php
@@ -38,7 +38,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
                         if ($pub_subaction != "infoserver") {
                             echo "\t\t\t" . "<td class='c' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=infoserver';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_GENERAL_INFO']."</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>".$lang['ADMIN_TITLE_GENERAL_INFO']."</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
@@ -50,7 +50,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
                         if ($pub_subaction != "parameter") {
                             echo "\t\t\t" . "<td class='c_tech' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=parameter';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_SERVER_CONF']."</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>".$lang['ADMIN_TITLE_SERVER_CONF']."</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
@@ -62,7 +62,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
                         if ($pub_subaction != "affichage") {
                             echo "\t\t\t" . "<td class='c' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=affichage';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_DISPLAY_CONF']."</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>".$lang['ADMIN_TITLE_DISPLAY_CONF']."</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
@@ -74,7 +74,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1 || $user_data["management_user"] == 1) {
                         if ($pub_subaction != "member") {
                             echo "\t\t\t" . "<td class='c' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=member';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_MEMBER_CONF']."</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>".$lang['ADMIN_TITLE_MEMBER_CONF']."</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
@@ -86,7 +86,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1 || $user_data["management_user"] == 1) {
                         if ($pub_subaction != "group") {
                             echo "\t\t\t" . "<td class='c' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=group';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_GROUP_CONF']."<</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>" .$lang['ADMIN_TITLE_GROUP_CONF']. "</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
@@ -98,7 +98,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
                         if ($pub_subaction != "viewer") {
                             echo "\t\t\t" . "<td class='c' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=viewer';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_LOGS_CONF']."</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>".$lang['ADMIN_TITLE_LOGS_CONF']."</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
@@ -110,7 +110,7 @@ require_once("views/page_header.php");
                     if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
                         if ($pub_subaction != "mod") {
                             echo "\t\t\t" . "<td class='c' width='14%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=mod';\">";
-                            echo "<a style='cursor:pointer'><font color='lime'>".$lang['ADMIN_TITLE_MODS_CONF']."</font></a>";
+                            echo "<a style='cursor:pointer;color: lime;'>".$lang['ADMIN_TITLE_MODS_CONF']."</a>";
                             echo "</td>" . "\n";
                         } else {
                             echo "\t\t\t" . "<th width='14%'>";
