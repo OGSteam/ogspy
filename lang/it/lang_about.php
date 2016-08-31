94e86cfec6e775d1e0f1f02ba942b3c647dc16b8
Anthony <darknoon29@users.noreply.github.com>
2016-08-31 22:04:13 +0200

    hgignore

---
+++ lang/it/lang_about.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * About Language File
+ * @package OGSpy
+ * @subpackage i18n
+ * @author DarkNoon
+ * @copyright Copyright &copy; 2016, http://ogsteam.fr/
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version 3.3.0
+ */
+
+/* about.php */
+$lang['ABOUT_TEAM'] = "L'OGSteam";
+$lang['ABOUT_CHANGELOG'] = "Lista delle modifiche";
+
+/*about_changelog.php*/
+
+$lang['ABOUT_RELEASENOTE'] = "Note di rilascio";
+$lang['ABOUT_VERSION'] = "Versione";
+$lang['ABOUT_DESCRIPTION'] = "Descrizione";
+
+/*about_ogsteam.php*/
+
+$lang['ABOUT_PROJECT_TEAM'] = "Team del progetto";
+$lang['ABOUT_PROJECT_AERIS'] = "Amministratore del sito OGSteam.fr";
+$lang['ABOUT_PROJECT_CAPI'] = "Amministratore Web / Sviluppatore / Tester di OGSteam.fr";
+$lang['ABOUT_PROJECT_CHRYS'] = "Artista grafico";
+$lang['ABOUT_PROJECT_DARKNOON'] = "Responsabile del progetto";
+$lang['ABOUT_PROJECT_DARKNOON1'] = "Sviluppatore Xtense - Script GreaseMonkey";
+$lang['ABOUT_PROJECT_JEDINIGHT'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_JEDINIGHT1'] = "Sviluppatore Moduli - Xtense, Xtense Firefox, Hostiles";
+$lang['ABOUT_PROJECT_MACHINE'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_NINETY'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_SHAD'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_SUPERBOX'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_XAVIERNUMA'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_PITCH314'] = "Sviluppatore";
+$lang['ABOUT_PROJECT_PITCH314_1'] = "Sviluppatore OGSpy";
+$lang['ABOUT_PROJECT_PITCH314_2'] = "Sviluppatore moduli - Build.Tech HOF, Production, Qui me sonde, Temps de vols";
+$lang['ABOUT_TEST_TEAM'] = "Team dei tester";
---
+++ lang/it/lang_message.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Message Language File
+ * @package OGSpy
+ * @subpackage i18n
+ * @author DarkNoon
+ * @copyright Copyright &copy; 2016, http://ogsteam.fr/
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version 3.3.0
+ */
+
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
+/* message.php */
+$lang['MSG_SYSTEM'] = "Message Système";
+$lang['MSG_FORBIDDEN'] = "Vous ne disposez pas des droits nécessaires pour effectuer cette action";
+$lang['MSG_ERRORFATAL'] = "Interruption suite à une erreur fatale.";
+$lang['MSG_ERRORMOD'] = "Problème pour installer ou mettre à jour le mod. Consultez le journal pour plus de détails.";
+$lang['MSG_ERRORDATA'] = "Les données transmises sont incorrectes";
+$lang['MSG_CREATE_USER_TITLE'] = "Création réussie du compte de";
+$lang['MSG_CREATE_USER_INFO'] = "Transmettez lui ces informations :";
+$lang['MSG_CREATE_USER_URL'] = "URL du serveur";
+$lang['MSG_CREATE_USER_PASSWORD'] = "Password";
+$lang['MSG_CREATE_USER_XTENSE'] = "URL Xtense";
+$lang['MSG_CREATE_USER_BBCODE'] = "BBCode pour";
+$lang['MSG_CREATE_USER_BBCODE_USER'] = "Utilisateur";
+$lang['MSG_PWD_REGEN_OK'] = "Génération réussie du nouveau mot de passe de";
+$lang['MSG_PWD_REGEN_INFO'] = "Transmettez lui son mot de passe :";
+$lang['MSG_PWD_REGEN_KO'] = "Génération du nouveau mot de passe échouée";
+$lang['MSG_NEW_ACCOUNT_KO'] = "Création du compte de échouée";
+$lang['MSG_NEW_ACCOUNT_KO_NAME'] = "Le pseudo est déjà utilisé";
+$lang['MSG_NEW_ACCOUNT_KO_NAME_ILLEGAL'] = "Le pseudo doit contenir entre 3 et 15 caractères standards";
+$lang['MSG_NEW_ACCOUNT_KO_PASSWORD_ILLEGAL'] = "Le mot de passe doit contenir entre 6 et 15 caractères";
+$lang['MSG_NEW_ACCOUNT_KO_OTHER'] = "Le pseudo est incorrect";
+$lang['MSG_PROFILE_OK'] = "Modification du profil réussie";
+$lang['MSG_PROFILE_KO'] = "Modification du profil échouée";
+$lang['MSG_PROFILE_SAVE_OK'] = "Sauvegarde de votre profil réussie";
+$lang['MSG_PROFILE_SAVE_KO'] = "Sauvegarde de votre profil échouée";
+$lang['MSG_PROFILE_SAVE_PWD'] = "Saisissez correctement votre ancien mot de passe et deux fois le nouveau";
+$lang['MSG_PROFILE_SAVE_PWD_ILLEGAL'] = "Le mot de passe doit contenir entre 6 et 15 caractères standards";
+$lang['MSG_PROFILE_SAVE_NAME_INUSE'] = "Le pseudo est déjà utilisé par un autre membre";
+$lang['MSG_PROFILE_SAVE_NAME_ILLEGAL'] = "Le pseudo saisi doit contenir entre 3 et 15 caractères standards";
+$lang['MSG_DELETE_USER_OK'] = "Suppression du membre réussie";
+$lang['MSG_DELETE_USER_KO'] = "Suppression du membre échouée";
+$lang['MSG_LOGIN_WRONG'] = "Vos identifiants de connexion sont incorrects";
+$lang['MSG_LOGIN_INACTIVE'] = "Votre compte n'est pas activé";
+$lang['MSG_LOGIN_INACTIVE_CONTACT'] = "Contactez un responsable";
+$lang['MSG_MAX_FAVORITES'] = "Vous avez atteint le nombre maximal de favoris";
+$lang['MSG_SETTINGS_SERVERCONFIG_OK'] = "Configuration des paramètres serveur achevée avec succès";
+$lang['MSG_SETTINGS_SERVERCONFIG_KO'] = "La configuration des paramètres serveur a échouée";
+$lang['MSG_SETTINGS_SERVERVIEW_OK'] = "Configuration des paramètres d'affichage achevée avec succès";
+$lang['MSG_SETTINGS_SERVERVIEW_KO'] = "La configuration des paramètres d'affichage a échouée";
+$lang['MSG_LOG_MISSING'] = "Il n'y a pas de fichiers logs à cette période";
+$lang['MSG_LOG_REMOVE'] = "Le fichier log séléctionné a bien été supprimé";
+$lang['MSG_FAILED_PLANETID'] = "Veuillez préciser la planète concernée";
+$lang['MSG_INSTALLFOLDER'] = "Veuillez supprimer le dossier 'install'";
+$lang['MSG_GROUP_CREATE'] = "Création du groupe réussie";
+$lang['MSG_GROUP_CREATE_FAILED'] = "Création du groupe échouée";
+$lang['MSG_GROUP_CREATE_FAILED_NAME'] = "Le nom est déjà utilisé";
+$lang['MSG_GROUP_CREATE_FAILED_ILLEGAL'] = "Le nom doit contenir entre 3 et 15 caractères standards";
+$lang['MSG_GROUP_CREATE_FAILED_OTHER'] = "Le nom est incorrect";
+$lang['MSG_DB_OPTIM_OK'] = "Optimisation terminée";
+$lang['MSG_DB_OPTIM_BEFORE'] = "Espace occupé avant optimisation";
+$lang['MSG_DB_OPTIM_AFTER'] = "Espace occupé après optimisation";
+$lang['MSG_EMPIRE_DATA_FAILURE'] = "Un problème est survenu durant l'acquisition de votre empire";
+$lang['MSG_RATIO_RAZ'] = "Remise à zéro des recherches effectuée.";
+$lang['MSG_BACK'] = "Indietro";
