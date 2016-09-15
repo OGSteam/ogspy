aac36975e43c513055415ecc2653464c46ca7c52
Anthony <darknoon29@users.noreply.github.com>
2016-09-15 21:40:24 +0200

    Group functions

--- model/Group_Model.php
+++ model/Group_Model.php
@@ -12,6 +12,10 @@ namespace Ogsteam\Ogspy\Model;
 class Group_Model
 {
 
+    /**
+     * @param $group_id
+     * @return \Ogsteam\Ogspy\the
+     */
     public function get_group_rights($group_id){
 
         global $db;
@@ -31,6 +35,9 @@ class Group_Model
 
     }
 
+    /**
+     * @return array
+     */
     public function get_group_list(){
         global $db;
         $request = "select group_id, group_name ";
@@ -46,7 +53,31 @@ class Group_Model
         return $info_usergroup;
     }
 
+    /**
+     * @param $group_id
+     * @return array
+     */
+    public function get_user_list($group_id)
+    {
+        global $db;
+        $usergroup_member = array();
 
+        $request = "SELECT u.user_id, u.user_name FROM " . TABLE_USER . " AS  u, " .
+            TABLE_USER_GROUP . " AS g";
+        $request .= " where u.user_id = g.user_id";
+        $request .= " and g.group_id = " . $group_id;
+        $request .= " order by user_name";
+        $result = $db->sql_query($request);
+        while ($row = $db->sql_fetch_assoc($result)) {
+            $usergroup_member[] = $row;
+        }
+        return $usergroup_member;
+    }
+
+    /**
+     * @param $name
+     * @return bool|\Ogsteam\Ogspy\Returs
+     */
     public function insert_group($name)
     {
         global $db;
@@ -65,12 +96,54 @@ class Group_Model
             return false;
     }
 
-    public function update_group()
+    /**
+     * @param $group_id
+     * @param string $name
+     * @param int $server_set_system
+     * @param int $server_set_spy
+     * @param int $server_set_rc
+     * @param int $server_set_ranking
+     * @param int $server_show_positionhided
+     * @param int $ogs_connection
+     * @param int $ogs_set_system
+     * @param int $ogs_get_system
+     * @param int $ogs_set_spy
+     * @param int $ogs_get_spy
+     * @param int $ogs_set_ranking
+     * @param int $ogs_get_ranking
+     */
+    public function update_group($group_id, $name, $server_set_system = 0, $server_set_spy = 0,
+                                 $server_set_rc = 0, $server_set_ranking = 0, $server_show_positionhided = 0,
+                                 $ogs_connection = 0, $ogs_set_system = 0, $ogs_get_system = 0, $ogs_set_spy = 0,
+                                 $ogs_get_spy = 0, $ogs_set_ranking = 0, $ogs_get_ranking = 0)
     {
         global $db;
 
+        $name = $db->sql_escape_string($name);
+
+        $request = "UPDATE " . TABLE_GROUP;
+        $request .= " SET group_name = '" . $name .
+            "',";
+        $request .= " server_set_system = '" . $server_set_system .
+            "', server_set_spy = '" . $server_set_spy .
+            "', server_set_rc = '" .  $server_set_rc .
+            "', server_set_ranking = '" . $server_set_ranking .
+            "', server_show_positionhided = '" . $server_show_positionhided .
+            "', ogs_connection = '" . $ogs_connection .
+            "', ogs_set_system = '" . $ogs_set_system .
+            "', ogs_get_system = '" . $ogs_get_system .
+            "', ogs_set_spy = '" . $ogs_set_spy .
+            "', ogs_get_spy = '" . $ogs_get_spy .
+            "', ogs_set_ranking = '" . $ogs_set_ranking .
+            "', ogs_get_ranking = '" . $ogs_get_ranking .
+            "'";
+        $request .= " WHERE group_id = " . $group_id;
+        $db->sql_query($request);
     }
 
+    /**
+     * @param $group_id
+     */
     public function delete_group($group_id)
     {
         global $db;
