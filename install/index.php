d85b022a639e0d478d5a0dcc2624ba86513321eb
Darknoon29 <darknoon@darkcity.fr>
2019-05-15 23:53:13 +0200

    Correctif #44
    #44 PHP 7.0 Requis

--- install/index.php
+++ install/index.php
@@ -79,7 +79,7 @@ if (isset($pub_redirection)) {
                 </tr>
                 <tr><td>&nbsp;</td></tr>
                 <?php
-                if (!(version_compare(PHP_VERSION, "5.2.0") >= 0)) {
+                if (!(version_compare(PHP_VERSION, "7.0.0") >= 0)) {
                     echo "<tr><td style='font-size: 24px;'><span style=\"color: red; \">" . $lang['INSTALL_PHPERROR'] . "</span></td></tr>";
                     echo "<tr><td><span style=\"color: red; \">" . $lang['INSTALL_PHP_ADVISE'];
                     echo "<br/><br/>" . $lang['INSTALL_PHPVERSION'] . PHP_VERSION;
