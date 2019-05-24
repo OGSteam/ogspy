e7c7c6a348f715a568ba70a87963038262f71657
machine <28401480+machine62@users.noreply.github.com>
2019-05-24 07:20:07 +0200

    Prepa release 3.3.7-alpha2

--- install/upgrade_to_latest.php
+++ install/upgrade_to_latest.php
@@ -300,7 +300,12 @@ switch ($ogsversion) {
         $ogsversion = '3.3.7-alpha1';
         $up_to_date = true;
 
+    case '3.3.7-alpha1':
+        $requests[] = "UPDATE " . TABLE_CONFIG . " SET config_value = '3.3.7-alpha2' WHERE config_name = 'version'";
+        $ogsversion = '3.3.7-alpha2';
+        $up_to_date = true;
         break;
+
     default:
     die("Aucune mise … jour n'est disponible");
 }
