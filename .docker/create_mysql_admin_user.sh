c63c64a8edc1483dfb21ba5ed49a0c86cdc80af5
Darknoon29 <darknoon@darkcity.fr>
2019-08-29 14:45:18 +0200

    Update create_mysql_admin_user.sh

--- .docker/create_mysql_admin_user.sh
+++ .docker/create_mysql_admin_user.sh
@@ -15,7 +15,8 @@ _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating MySQL admin user with ${_word} password"
 
 mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'127.0.0.1' WITH GRANT OPTION"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED VIA unix_socket WITH GRANT OPTION;"
 mysql -uroot -e "CREATE DATABASE ogspy"
 # You can create a /mysql-setup.sh file to intialized the DB
 if [ -f /mysql-setup.sh ] ; then
