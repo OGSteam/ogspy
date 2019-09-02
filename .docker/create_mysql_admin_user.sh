6dce16b355a11134b9d9548b87351223fc474e58
Darknoon29 <darknoon@darkcity.fr>
2019-09-02 14:51:52 +0200

    With Grant

--- .docker/create_mysql_admin_user.sh
+++ .docker/create_mysql_admin_user.sh
@@ -15,7 +15,7 @@ _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating MySQL admin user with ${_word} password"
 
 mysql -uroot -e "CREATE USER 'admin'@'127.0.0.1' IDENTIFIED VIA mysql_native_password USING PASSWORD('$PASS')"
-#mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'127.0.0.1' WITH GRANT OPTION"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'127.0.0.1' WITH GRANT OPTION"
 #mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED VIA unix_socket WITH GRANT OPTION"
 
 mysql -uroot -e "CREATE DATABASE ogspy"
