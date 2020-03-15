44e31571a8d3319fed79d459ffa444841a68c607
machine62 <28401480+machine62@users.noreply.github.com>
2020-03-15 11:20:40 +0100

    reecriture footer

--- views/page_foot.php
+++ views/page_foot.php
@@ -34,6 +34,32 @@ if (is_array($ogspy_phperror) && count($ogspy_phperror)) {
 
 
 <!-- Place pour bas de pages -->
+
+
+
+<div id="footbarre">
+    <div class="toologs">
+        <p>
+            <a href="https://www.ogsteam.fr" target="_blank">OGSpy</a> <span class="version"><?php echo $server_config["version"] ?></span></span> <?php echo $lang['FOOTER_OGSPY'] ?> OGSteam &copy; 2005-2020
+        </p>
+    </div>
+    <div class="online">
+        <p>
+            <span class="nb_users"><?php echo $nb_users ?></span> <?php echo $lang['FOOTER_CONTRIBUTORS'] . (($nb_users > 1) ? "s" : "") . " " . $lang['FOOTER_ON_SITE'] ?>
+        </p>
+    </div>
+    <div class="bench">
+        <p>
+            <?php echo $lang['FOOTER_RENDERING'] ;?> <span class="value"><?php echo round($php_timing + $sql_timing, 3); ?></span> sec (<span class="language">PHP</span> : <span class="value"><?php echo round($php_timing, 3); ?></span> / <span class=language">SQL</span> : <span class="value"><?php echo round($sql_timing, 3); ?></span>)
+            [ <span class="value"><?php echo $nb_requete;?></span> <?php echo $lang['FOOTER_QUERY'] . (($nb_requete > 1) ? "s" : ""); ?>]
+        </p>
+    </div>
+
+
+</div>
+
+
+<!--
 <div id='barre'>
     <table style="width:100%">
         <tr>
@@ -55,7 +81,7 @@ if (is_array($ogspy_phperror) && count($ogspy_phperror)) {
     </table>
 
 </div>
-
+-->
 <?php echo (new ToolTip_Helper())->GetHTMLHideContent(); ?>
 <?php echo (new ToolTip_Helper())->activateJs(); ?>
 
