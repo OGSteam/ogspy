bfa1ba4ac863810400e3b842defce7d5fe151e45
machine62 <28401480+machine62@users.noreply.github.com>
2020-03-20 19:43:06 +0100

    Utilisation helper
    Changement menu horizontal pages admin

--- views/admin.php
+++ views/admin.php
@@ -14,6 +14,9 @@
 if (!defined('IN_SPYOGAME')) {
     die("Hacking attempt");
 }
+use Ogsteam\Ogspy\Helper\html_ogspy_Helper;
+
+
 // Verification des droits admins
 if ($user_data["user_admin"] != 1 && $user_data["user_coadmin"] != 1 && $user_data["management_user"] != 1) {
     redirection("index.php?action=message&amp;id_message=forbidden&amp;info");
@@ -22,161 +25,160 @@ if ($user_data["user_admin"] != 1 && $user_data["user_coadmin"] != 1 && $user_da
 require_once("views/page_header.php");
 ?>
 
-<table width="100%">
-    <tr>
-        <td>
-            <table border="1" width="100%">
-                <tr align="center">
-                    <?php
-                    if (!isset($pub_subaction)) {
-                        if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                            $pub_subaction = "infoserver";
-                        } else {
-                            $pub_subaction = "member";
-                        }
-                    }
+<?php
+if (!isset($pub_subaction)) {
+    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
+        $pub_subaction = "infoserver";
+    } else {
+        $pub_subaction = "member";
+    }
+}
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                        if ($pub_subaction != "infoserver") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=infoserver';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_GENERAL_INFO'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_GENERAL_INFO'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                        if ($pub_subaction != "parameter") {
-                            echo "\t\t\t" . "<td class='c_tech' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=parameter';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_SERVER_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_SERVER_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
+$menuLinks = array();
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                        if ($pub_subaction != "affichage") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=affichage';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_DISPLAY_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_DISPLAY_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
+//utilisateur admin ou coadmin
+if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1 || $user_data["management_user"] == 1) {
-                        if ($pub_subaction != "member") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=member';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_MEMBER_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_MEMBER_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
+    // INFOSERVEUR
+    $infoserver = array();
+    $infoserver[ "tag"] = "infoserver actif";
+    $infoserver["content"] = $lang['ADMIN_TITLE_GENERAL_INFO'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "infoserver") {
+        $infoserver["tag"] = "infoserver";
+        $infoserver["url"]= "index.php?action=administration&amp;subaction=infoserver";
+     }
+    $menuLinks[]=$infoserver;
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1 || $user_data["management_user"] == 1) {
-                        if ($pub_subaction != "group") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=group';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_GROUP_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_GROUP_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
+    //PARAMETER
+    $parameter =array();
+    $parameter[ "tag"] = "parameter actif";
+    $parameter["content"] = $lang['ADMIN_TITLE_SERVER_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "parameter") {
+        $parameter["tag"] = "parameter";
+        $parameter["url"]= "index.php?action=administration&amp;subaction=parameter";
+    }
+    $menuLinks[]=$parameter;
+    //AFFICHAGE
+    $affichage =array();
+    $affichage[ "tag"] = "affichage actif";
+    $affichage["content"] = $lang['ADMIN_TITLE_DISPLAY_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "affichage") {
+        $affichage["tag"] = "affichage";
+        $affichage["url"]= "index.php?action=administration&amp;subaction=affichage";
+    }
+    $menuLinks[]=$affichage;
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                        if ($pub_subaction != "viewer") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=viewer';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_LOGS_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_LOGS_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
+}
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                        if ($pub_subaction != "helper") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=helper';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_HELPER_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_HELPER_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
+//utilisateur admin ou coadmin ou geston membre
+if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1 || $user_data["management_user"] == 1) {
+    //MEMBER
+    $member =array();
+    $member[ "tag"] = "member actif";
+    $member["content"] = $lang['ADMIN_TITLE_MEMBER_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "member") {
+        $member["tag"] = "member";
+        $member["url"]= "index.php?action=administration&amp;subaction=member";
+    }
+    $menuLinks[]=$member;
+    //GROUP
+    $group =array();
+    $group[ "tag"] = "group actif";
+    $group["content"] = $lang['ADMIN_TITLE_GROUP_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "group") {
+        $group["tag"] = "group";
+        $group["url"]= "index.php?action=administration&amp;subaction=group";
+    }
+    $menuLinks[]=$group;
+}
 
-                    if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
-                        if ($pub_subaction != "mod") {
-                            echo "\t\t\t" . "<td class='c' width='12%' onclick=\"window.location = 'index.php?action=administration&amp;subaction=mod';\">";
-                            echo "<a style='cursor:pointer;color: lime;'>" . $lang['ADMIN_TITLE_MODS_CONF'] . "</a>";
-                            echo "</td>" . "\n";
-                        } else {
-                            echo "\t\t\t" . "<th width='12%'>";
-                            echo "<a>" . $lang['ADMIN_TITLE_MODS_CONF'] . "</a>";
-                            echo "</th>" . "\n";
-                        }
-                    }
-                    ?>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <?php
-            switch ($pub_subaction) {
-                case "member" :
-                    require_once("admin_members.php");
-                    break;
+//A nouveau utilisateur admin ou coadmin
+if ($user_data["user_admin"] == 1 || $user_data["user_coadmin"] == 1) {
+    //VIEWER
+    $viewer =array();
+    $viewer[ "tag"] = "viewer actif";
+    $viewer["content"] = $lang['ADMIN_TITLE_GROUP_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "viewer") {
+        $viewer["tag"] = "viewer";
+        $viewer["url"]= "index.php?action=administration&amp;subaction=viewer";
+    }
+    $menuLinks[]=$viewer;
+    //HELPER
+    $helper =array();
+    $helper[ "tag"] = "helper actif";
+    $helper["content"] = $lang['ADMIN_TITLE_HELPER_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "helper") {
+        $helper["tag"] = "helper";
+        $helper["url"]= "index.php?action=administration&amp;subaction=helper";
+    }
+    $menuLinks[]=$helper;
+    //mod
+    $mod =array();
+    $mod[ "tag"] = "mod actif";
+    $mod["content"] = $lang['ADMIN_TITLE_MODS_CONF'];
+    // si page deja affichée, on met le lien qui va bien
+    if ($pub_subaction != "mod") {
+        $mod["tag"] = "mod";
+        $mod["url"]= "index.php?action=administration&amp;subaction=mod";
+    }
+    $menuLinks[]=$mod;
 
-                case "group" :
-                    require_once("admin_members_group.php");
-                    break;
 
-                case "parameter" :
-                    require_once("admin_parameters.php");
-                    break;
 
-                case "affichage" :
-                    require_once("admin_affichage.php");
-                    break;
+}
 
-                case "viewer" :
-                    require_once("admin_viewer.php");
-                    break;
 
-                case "helper" :
-                    require_once("admin_helper.php");
-                    break;
 
-                case "mod" :
-                    require_once("admin_mod.php");
-                    break;
+echo (new html_ogspy_Helper())->navbarreMenu("admin" ,$menuLinks );
 
-                default:
-                    require_once("admin_infoserver.php");
-                    break;
-            }
-            ?>
-        </td>
-    </tr>
-</table>
 
+?>
+
+
+<?php
+switch ($pub_subaction) {
+    case "member" :
+        require_once("admin_members.php");
+        break;
+
+    case "group" :
+        require_once("admin_members_group.php");
+        break;
+
+    case "parameter" :
+        require_once("admin_parameters.php");
+        break;
+
+    case "affichage" :
+        require_once("admin_affichage.php");
+        break;
+
+    case "viewer" :
+        require_once("admin_viewer.php");
+        break;
+
+    case "helper" :
+        require_once("admin_helper.php");
+        break;
+
+    case "mod" :
+        require_once("admin_mod.php");
+        break;
+
+    default:
+        require_once("admin_infoserver.php");
+        break;
+}
+
+?>
 <?php
 require_once("views/page_tail.php");
 ?>
