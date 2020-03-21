4409e8dd1c82b784c7503e8180ab4543f2710396
machine62 <28401480+machine62@users.noreply.github.com>
2020-03-21 08:07:29 +0100

    oubli import use

--- views/page_foot.php
+++ views/page_foot.php
@@ -6,6 +6,7 @@
 if (!defined('IN_SPYOGAME')) {
     die("Hacking attempt");
 }
+use Ogsteam\Ogspy\Helper\ToolTip_Helper;
 ?>
 
 
@@ -59,29 +60,6 @@ if (is_array($ogspy_phperror) && count($ogspy_phperror)) {
 </div>
 
 
-<!--
-<div id='barre'>
-    <table style="width:100%">
-        <tr>
-            <td></td>
-        </tr>
-        <tr>
-            <td style="width:33%;text-align:left;font-size:11px;font-style:italic">
-                <a style="font-weight:bold;font-size: 12px" href="https://www.ogsteam.fr"
-                   target="_blank">OGSpy</a> <?php echo $server_config["version"] . " " . $lang['FOOTER_OGSPY']; ?> OGSteam &copy; 2005-2020<br/>
-            </td>
-            <td style="width:34%;text-align:center;font-size:11px;font-style:italic;font-weight:bold;"><?php echo("<span id='nb_users'>" . $nb_users . "</span> " . $lang['FOOTER_CONTRIBUTORS'] . (($nb_users > 1) ? "s" : "") . " " . $lang['FOOTER_ON_SITE']); ?></td>
-            <td style="width:33%;text-align:right;font-size:11px;font-style:italic">
-                <?php echo $lang['FOOTER_RENDERING'] . " " . round($php_timing + $sql_timing, 3); ?> sec (<span
-                    style="font-weight:bold;">PHP</span> : <?php echo round($php_timing, 3); ?> / <span
-                    style="font-weight:bold;">SQL</span> : <?php echo round($sql_timing, 3); ?>)
-                [<?php echo ($nb_requete . " " . $lang['FOOTER_QUERY'] . (($nb_requete > 1) ? "s" : "")); ?>]
-            </td>
-        </tr>
-    </table>
-
-</div>
--->
 <?php echo (new ToolTip_Helper())->GetHTMLHideContent(); ?>
 <?php echo (new ToolTip_Helper())->activateJs(); ?>
 
