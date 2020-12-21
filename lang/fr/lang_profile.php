110277c10824fdc4ee5efc0bc8b2eb73a8dd2cdf
pitch314 <8677243+pitch314@users.noreply.github.com>
2020-12-20 22:27:51 -1000

    Correction encodage texte d'erreur.

--- lang/fr/lang_profile.php
+++ lang/fr/lang_profile.php
@@ -17,7 +17,7 @@
 /* profile.php */
 
 $lang['PROFILE_ERROR_RETRY'] = "Saisissez le nouveau mot de passe et sa confirmation";
-$lang['PROFILE_ERROR_OLDPWD'] = "Saisissez l'ancien mot de passe";
+$lang['PROFILE_ERROR_OLDPWD'] = "Saisissez l&#146;ancien mot de passe";
 $lang['PROFILE_ERROR_ERROR'] = "Le mot de passe saisi est différent de la confirmation";
 $lang['PROFILE_ERROR_ILLEGAL'] = "Le mot de passe doit contenir entre 6 et 15 caractères";
 
