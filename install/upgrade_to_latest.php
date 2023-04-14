2b4aadbda972ae4aabdde574b805686b3307c65b
Anthony <13015521+darknoon29@users.noreply.github.com>
2023-04-14 18:00:04 +0200

    Another Update to update script

--- install/upgrade_to_latest.php
+++ install/upgrade_to_latest.php
@@ -77,8 +77,8 @@ switch ($ogsversion) {
         $requests[] = "ALTER TABLE `" . TABLE_ROUND_DEFENSE . "` ADD `ECL` SMALLINT(2) NOT NULL DEFAULT '-1' AFTER `TRA`";
         $requests[] = "ALTER TABLE `" . TABLE_ROUND_DEFENSE . "` ADD `FOR` SMALLINT(2) NOT NULL DEFAULT '-1' AFTER `SAT`";
 
-
     case '3.3.7':
+        // New Table
         $requests[] = "CREATE TABLE IF NOT EXISTS `" . TABLE_MOD_USER_CFG . "` (
                         `mod`     VARCHAR(50) NOT NULL,
                         `user_id` INT(11) NOT NULL,
@@ -87,19 +87,22 @@ switch ($ogsversion) {
                         PRIMARY KEY (`mod`, `user_id`, `config`),
                         UNIQUE KEY `config` (`config`)
                     ) DEFAULT CHARSET = UTF8;";
-        $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('ddr','1')";
-        $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('astro_strict','1')";
+
         $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('donutSystem','1')";
         $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('donutGalaxy','1')";
         $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('speed_fleet_peaceful','1')";
         $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('speed_fleet_war','1')";
         $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('speed_fleet_holding','1')";
         $requests[] = "INSERT INTO " . TABLE_CONFIG . " VALUES ('speed_research_divisor','1')";
+
+
         $requests[] = "ALTER TABLE `" . TABLE_USER . "` ADD `user_pwd_change` TINYINT(1) NOT NULL DEFAULT '1' AFTER `user_password_s`";
         $requests[] = "UPDATE " . TABLE_USER . " SET `user_pwd_change` = '0' WHERE `user_pwd_change` = '1'";    //Ne pas impacter les users existant
         $requests[] = "ALTER TABLE `" . TABLE_USER . "` ADD `user_email_valid` TINYINT(1) NOT NULL DEFAULT '0' AFTER `user_email`";
+        $requests[] = "ALTER TABLE " . TABLE_USER . " ADD `ally_class` ENUM ('none', 'MAR', 'WAR', 'RES') NOT NULL DEFAULT 'none' AFTER `user_class`";
 
         $requests[] = "ALTER TABLE " . TABLE_RANK_PLAYER_MILITARY . " MODIFY `nb_spacecraft` BIGINT";
+
         $requests[] = "ALTER TABLE " . TABLE_PARSEDRCROUND . " MODIFY `attaque_tir` BIGINT";
         $requests[] = "ALTER TABLE " . TABLE_PARSEDRCROUND . " MODIFY `attaque_puissance` BIGINT";
         $requests[] = "ALTER TABLE " . TABLE_PARSEDRCROUND . " MODIFY `defense_bouclier` BIGINT";
@@ -109,10 +112,9 @@ switch ($ogsversion) {
 
         $requests[] = "ALTER TABLE " . TABLE_GAME_ALLY . " ADD `class` ENUM ('none', 'MAR', 'WAR', 'RES') NOT NULL DEFAULT 'none' AFTER `tag`";
         $requests[] = "ALTER TABLE " . TABLE_GAME_PLAYER . " ADD `class` ENUM ('none', 'COL', 'GEN', 'EXP') NOT NULL DEFAULT 'none' AFTER `status`";
-        $requests[] = "ALTER TABLE " . TABLE_USER . " ADD `ally_class` ENUM ('none', 'MAR', 'WAR', 'RES') NOT NULL DEFAULT 'none' AFTER `user_class`";
+
 
         $requests[] = "ALTER TABLE " . TABLE_MOD . " MODIFY `version` VARCHAR(100) NOT NULL";
-
         $requests[] = "ALTER TABLE " . TABLE_USER_BUILDING . " MODIFY `boosters` VARCHAR(64) NOT NULL DEFAULT 'm:0:0_c:0:0_d:0:0_e:0:0_p:0_m:0'";
 
 
