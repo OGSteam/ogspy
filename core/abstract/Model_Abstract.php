22479429eaaf8df5a3a73322c4469329b14de39d
DarkNoon <13015521+darknoon29@users.noreply.github.com>
2023-05-29 14:43:08 +0200

    Apply Code Coverage Config

--- core/abstract/Model_Abstract.php
+++ core/abstract/Model_Abstract.php
@@ -12,23 +12,34 @@ abstract class Model_Abstract
 {
     protected $db;
 
+    /**
+     * Constructs a new instance of the class and initializes the database connection.
+     *
+     * @throws Some_Exception_Class description of exception
+     */
     public function __construct()
     {
         global $db; //todo prevoir un get_instance mysql
         $this->db = $db;
     }
 
+    /**
+     * Returns the ID generated from the previous INSERT operation.
+     *
+     * @return int The ID generated from the previous INSERT operation.
+     */
     function sql_insertid()
     {
         return $this->db->sql_insertid();
     }
 
+    /**
+     * Returns the number of rows affected by the previous SQL operation.
+     *
+     * @return int The number of affected rows.
+     */
     function sql_affectedrows()
     {
         return $this->db->sql_affectedrows();
     }
-
-
-
-
 }
