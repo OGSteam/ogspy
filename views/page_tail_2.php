018d11b20c65bf8d4d03c3c6bf913021b0c8616a
machine62 <28401480+machine62@users.noreply.github.com>
2023-09-17 08:05:03 +0200

    Redesign message
    modification page header et footer 2
    message defautl/warning/danger/success

--- views/page_tail_2.php
+++ views/page_tail_2.php
@@ -19,6 +19,8 @@ if (!defined('IN_SPYOGAME')) {
 use Ogsteam\Ogspy\Helper\ToolTip_Helper;
 use Ogsteam\Ogspy\Helper\Benchmark_Helper;
 
+$nb_requete = $db->nb_requete;
+$nb_users = user_get_nb_active_users();
 $db->sql_close(); // fermeture de la connexion à la base de données
 $benchogspy->stop("fin");// Arret calcul temps
 
@@ -26,32 +28,42 @@ $ogspy_timing = $benchogspy->getAllElapsed(); // temps total
 $sql_timing = $benchSQL->getAllElapsed(); //temps sql
 $php_timing = $ogspy_timing - $sql_timing ; // delta => temps php
 
+
+
 ?>
-</td>
-</tr>
+</section><!-- Fin Contenu principal compat legacy -->
 <?php
-
 global $ogspy_phperror;
-
 if (is_array($ogspy_phperror) && count($ogspy_phperror)) {
     echo "\n<tr>\n\t<td><table><tr><th>" . $lang['FOOTER_PHPERRORS'] . "</th></tr>";
-
     foreach ($ogspy_phperror as $line) {
         echo "\n<tr><td>$line</td></tr>";
     }
-
     echo "</table>\n\t</td>\n</tr>";
 }
-
 ?>
-<tr>
-    <td style="color: #ECFF00; text-align:center; font-size:13px; font-style:italic">
-        <a style="font-weight:bold;" href="https://www.ogsteam.eu">OGSpy</a> is an <span style="font-weight:bold;">OGSteam Software</span> &copy;2005-2023<br />
-        <span style="font-style:normal;">v <?php echo $server_config["version"]; ?></span><br />
-        <?php echo ($lang['FOOTER_RENDERING']); ?> <?php echo round($php_timing + $sql_timing, 3); ?> sec (<span style="font-weight:bold;">PHP</span> : <?php echo round($php_timing, 3); ?> / <span style="font-weight:bold;">SQL</span> : <?php echo round($sql_timing, 3); ?>)<br />
-    </td>
-</tr>
-</table>
+
+<!--todo revoir footer de ce type de page -->
+<footer id='barre'>
+    <table style="width:100%">
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="width:33%;text-align:left;font-size:11px;font-style:italic">
+                <a style="font-weight:bold;font-size: 12px" href="https://www.ogsteam.eu" target="_blank" rel="noopener">OGSpy</a> <?php echo $server_config["version"] . " " . $lang['FOOTER_OGSPY']; ?> OGSteam &copy; 2005-2023<br />
+            </td>
+            <td style="width:34%;text-align:center;font-size:11px;font-style:italic;font-weight:bold;"><?php echo ("<span id='nb_users'>" . $nb_users . "</span> " . $lang['FOOTER_CONTRIBUTORS'] . (($nb_users > 1) ? "s" : "") . " " . $lang['FOOTER_ON_SITE']); ?></td>
+            <td style="width:33%;text-align:right;font-size:11px;font-style:italic">
+                <?php echo $lang['FOOTER_RENDERING'] . " " . round($php_timing + $sql_timing, 3); ?> sec (<span style="font-weight:bold;">PHP</span> : <?php echo round($php_timing, 3); ?> / <span style="font-weight:bold;">SQL</span> : <?php echo round($sql_timing, 3); ?>)
+                [<?php echo ($nb_requete . " " . $lang['FOOTER_QUERY'] . (($nb_requete > 1) ? "s" : "")); ?>]
+            </td>
+        </tr>
+
+    </table>
+
+</footer> <!-- fin pied de page footer html -->
+<?php echo (new ToolTip_Helper())->GetHTMLHideContent(); ?>
 </body>
 
 </html>
