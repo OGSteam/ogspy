b7cc31d51ef6ecb8f42987827ac9c07ae18f9814
machine62 <28401480+machine62@users.noreply.github.com>
2023-10-07 12:08:16 +0200

    Page Home_spy
    redesign OK

--- views/home_spy.php
+++ views/home_spy.php
@@ -16,7 +16,7 @@ if (!defined('IN_SPYOGAME')) {
 }
 
 $favorites = user_getfavorites_spy();
-
+// todo : verifier fonctionnement $sort et $sort2 
 if (!isset($sort2)) {
     $sort2 = 0;
 } else {
@@ -24,72 +24,109 @@ if (!isset($sort2)) {
 }
 ?>
 
-<table align="center">
-    <tr>
-        <td class="c" width="75"><a href="index.php?action=home&amp;subaction=spy&amp;sort=1&amp;sort2=<?php echo $sort2; ?>"><?php echo ($lang['HOME_SPY_POSITIONS']); ?></a>
-        </td>
-        <td class="c" width="120"><a href="index.php?action=home&amp;subaction=spy&amp;sort=2&amp;sort2=<?php echo $sort2; ?>"><?php echo ($lang['HOME_SPY_ALLIANCES']); ?></a>
-        </td>
-        <td class="c" width="120"><a href="index.php?action=home&amp;subaction=spy&amp;sort=3&amp;sort2=<?php echo $sort2; ?>"><?php echo ($lang['HOME_SPY_PLAYERS']); ?></a>
-        </td>
-        <td class="c" width="20"><a href="index.php?action=home&amp;subaction=spy&amp;sort=4&amp;sort2=<?php echo $sort2; ?>"><?php echo ($lang['HOME_SPY_MOON']); ?></a></td>
-        <td class="c" width="20">&nbsp;</td>
-        <td class="c" width="250"><a href="index.php?action=home&amp;subaction=spy&amp;sort=5&amp;sort2=<?php echo $sort2; ?>"><?php echo ($lang['HOME_SPY_UPDATE']); ?></a></td>
-        <td class="c" width="40">&nbsp;</td>
-        <td class="c" width="120">&nbsp;</td>
-    </tr>
-    <?php
-    foreach ($favorites as $v) {
-        $spy_id = $v["spy_id"];
-        $galaxy = $v["spy_galaxy"];
-        $system = $v["spy_system"];
-        $row = $v["spy_row"];
-        $player = $v["player"];
-        $ally = $v["ally"];
-        $moon = $v["moon"];
-        $status = $v["status"];
-        $timestamp = $v["datadate"];
+<table class="og-table og-medium-table">
+    <thead>
+        <tr>
+            <th>
+                <a href="index.php?action=home&amp;subaction=spy&amp;sort=1&amp;sort2=<?php echo $sort2; ?>">
+                    <?php echo ($lang['HOME_SPY_POSITIONS']); ?>
+                </a>
+            </th>
+            <th>
+                <a href="index.php?action=home&amp;subaction=spy&amp;sort=2&amp;sort2=<?php echo $sort2; ?>">
+                    <?php echo ($lang['HOME_SPY_ALLIANCES']); ?>
+                </a>
+            </th>
+            <th>
+                <a href="index.php?action=home&amp;subaction=spy&amp;sort=3&amp;sort2=<?php echo $sort2; ?>">
+                    <?php echo ($lang['HOME_SPY_PLAYERS']); ?>
+                </a>
+            </th>
+            <th>
+                <a href="index.php?action=home&amp;subaction=spy&amp;sort=4&amp;sort2=<?php echo $sort2; ?>">
+                    <?php echo ($lang['HOME_SPY_MOON']); ?>
+                </a>
+            </th>
+            <th>
+                &nbsp;
+            </th>
+            <th>
+                <a href="index.php?action=home&amp;subaction=spy&amp;sort=5&amp;sort2=<?php echo $sort2; ?>">
+                    <?php echo ($lang['HOME_SPY_UPDATE']); ?>
+                </a>
+            </th>
+            <th>
+                &nbsp;
+            </th>
+            <th>
+                &nbsp;
+            </th>
+    </thead>
+    <tbody>
 
-        if ($timestamp != 0) {
-            $timestamp = date("d M Y H:i", $timestamp);
-            $poster = $timestamp . " - " . $v["poster"];
-        }else {
-            $poster = $v["poster"];
-        }
+        <?php foreach ($favorites as $v) : ?>
+            <tr>
+                <td>
+                    <?php echo $v["spy_galaxy"] . ":" . $v["spy_system"] . ":" . $v["spy_row"]; ?>
+                </td>
+                <td>
+                    <?php if ($v["ally"] == "") : ?>
+                        &nbsp;
+                    <?php else : ?>
+                        <a href='index.php?action=search&amp;type_search=ally&amp;string_search=<?php echo $v["ally"]; ?>&strict=on'>
+                            <?php echo $v["ally"]; ?>
+                        </a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if ($v["player"] == "") : ?>
+                        &nbsp;
+                    <?php else : ?>
+                        <a href='index.php?action=search&amp;type_search=player&amp;string_search=<?php echo $v["player"]; ?>&strict=on'>
+                            <?php echo  $v["player"]; ?>
+                        </a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if ($v["moon"] == "") : ?>
+                        &nbsp;
+                    <?php else : ?>
+                        <span class="ogame-icon-moon">
+                         M
+                        </span>
+                        </a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if ($v["status"] == "") : ?>
+                        &nbsp;
+                    <?php else : ?>
+                        <span class="ogame-status-<?php echo $v["status"]; ?>">
+                            <?php echo $v["status"]; ?>
+                        </span>
+                        </a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if ($v["datadate"]!= 0) : ?>
+                        <?php echo date("d M Y H:i", $v["datadate"]) . " - " . $v["poster"]; ?>
 
-        if ($ally == "") {
-            $ally = "&nbsp;";
-        } else {
-            $ally = "<a href='index.php?action=search&amp;type_search=ally&amp;string_search=" . $ally . "&strict=on'>" . $ally . "</a>";
-        }
+                    <?php else : ?>
+                        <?php echo $v["poster"]; ?>
+                    <?php endif; ?>
+                </td>
 
-        if ($player == "") {
-            $player = "&nbsp;";
-        } else {
-            $player = "<a href='index.php?action=search&amp;type_search=player&amp;string_search=" . $player . "&strict=on'>" . $player . "</a>";
-        }
+                <td>
+                    <input class="og-button og-button-little" type='button' value='<?php echo $lang['HOME_SPY_SEE'];?>' onclick="window.open('index.php?action=show_reportspy&amp;galaxy=<?php echo $v['spy_galaxy'];?>&amp;system=<?php echo $v['spy_system'];?>&amp;row=<?php echo $v['spy_row'];?>&amp;spy_id=<?php echo $v['spy_id'];?>','_blank','width=640, height=480, toolbar=0, location=0, directories=0, status=0, scrollbars=1, resizable=1, copyhistory=0, menuBar=0');return(false)" />
+                </td>
+                <td>
+                    <!-- todo del_favorite_spy ne semble pas fonctionner -->
+                    <input class="og-button og-button-danger og-button-little"type='button' value='<?php echo $lang['HOME_SPY_FAVDELETE'];?>' onclick="window.location = 'index.php?action=del_favorite_spy&amp;spy_id=<?php echo $v['spy_id'];?>&amp;info=1';" />
+                </td>
 
-        if ($status == "") {
-            $status = " &nbsp;";
-        }
-
-        if ($moon == 1) {
-            $moon = " M";
-        } else {
-            $moon = "&nbsp;";
-        }
-
-        echo "<tr>";
-        echo "<th>$galaxy:$system:$row</th>";
-        echo "<th>$ally</th>";
-        echo "<th>$player</th>";
-        echo "<th>$moon</th>";
-        echo "<th>$status</th>";
-        echo "<th>$poster</th>";
-        $coords = explode(":", $row);
-        echo "<th><input type='button' value='" . $lang['HOME_SPY_SEE'] . "' onclick=\"window.open('index.php?action=show_reportspy&amp;galaxy=$galaxy&amp;system=$system&amp;row=$row&amp;spy_id=$spy_id','_blank','width=640, height=480, toolbar=0, location=0, directories=0, status=0, scrollbars=1, resizable=1, copyhistory=0, menuBar=0');return(false)\" /></th>";
-        echo "<th><input type='button' value='" . $lang['HOME_SPY_FAVDELETE'] . "' onclick=\"window.location = 'index.php?action=del_favorite_spy&amp;spy_id=$spy_id&amp;info=1';\" /></th>";
-        echo "</tr>";
-    }
-    ?>
+                
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table>
+
