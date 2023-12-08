e1509549cbecf44297c3eac7b0fb706c792ded86
DarkNoon <13015521+darknoon29@users.noreply.github.com>
2023-12-08 14:44:05 +0100

    Update Security for Password Generation

---
+++ tests/unit/FunctionsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionsTest extends TestCase
+{
+    public function testPasswordGenerator()
+    {
+        define("IN_SPYOGAME", true);
+        require_once 'includes/functions.php';
+        $password = password_generator();
+
+        // Assert that the password is a string
+        $this->assertIsString($password);
+
+        // Assert that the password length is 6
+        $this->assertEquals(8, strlen($password));
+
+        echo $password;
+    }
+}
