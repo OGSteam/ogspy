0a376699bce4b2279b64f9bdf706588143829456
DarkNoon <13015521+darknoon29@users.noreply.github.com>
2025-08-30 11:34:39 +0200

    Update version numbers in install/version.php
    Changed $ogspy_version to 0.0.0-dev and $database_version to 20250001001, possibly to reset or prepare for a new development cycle.

--- install/version.php
+++ install/version.php
@@ -9,5 +9,5 @@
  * @license https://opensource.org/licenses/gpl-license.php GNU Public License
  * @version 4.0.0
  */
-$ogspy_version = "4.0.0-dev";
-$database_version = "20250713001";
+$ogspy_version = "0.0.0-dev";
+$database_version = "20250001001";
