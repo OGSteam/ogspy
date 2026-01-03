4f60127e772938c715c7054a7bee03f6338923fc
machine62 <28401480+machine62@users.noreply.github.com>
2026-01-03 08:07:57 +0100

    Update ogspy_20251227001_UpgradeTo403.sql
    Ajout if exist sur les modifications

--- install/schemas/ogspy_20251227001_UpgradeTo403.sql
+++ install/schemas/ogspy_20251227001_UpgradeTo403.sql
@@ -14,42 +14,44 @@ ALTER TABLE `ogspy_mod_config` CHANGE `config` `config` VARCHAR(190) CHARACTER S
 ALTER TABLE `ogspy_mod_user_config` CHANGE `config` `config` VARCHAR(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL; 
 
 -- Suppression champs points_per_member/ally classement alliance
--- points_per_member
-ALTER TABLE `ogspy_game_rank_ally_military_loose` DROP `points_per_member`;
-ALTER TABLE `ogspy_game_rank_ally_military_destruct` DROP `points_per_member`;
-ALTER TABLE `ogspy_game_rank_ally_military_built` DROP `points_per_member`;
-ALTER TABLE `ogspy_game_rank_ally_military` DROP `points_per_member`;
-ALTER TABLE `ogspy_game_rank_ally_economics` DROP `points_per_member`;
-ALTER TABLE `ogspy_game_rank_ally_technology` DROP `points_per_member`;
-ALTER TABLE `ogspy_game_rank_ally_points` DROP `points_per_member`;
+-- points_per_member+
+ALTER TABLE `ogspy_game_rank_ally_honor` DROP  IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_military_loose` DROP  IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_military_destruct` DROP  IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_military_built` DROP  IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_military` DROP  IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_economics` DROP IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_technology` DROP  IF EXISTS `points_per_member`;
+ALTER TABLE `ogspy_game_rank_ally_points` DROP  IF EXISTS `points_per_member`;
 -- ally
-ALTER TABLE `ogspy_game_rank_ally_military_loose` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_ally_military_destruct` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_ally_military_built` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_ally_military` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_ally_economics` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_ally_technology` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_ally_points` DROP `ally`;
+ALTER TABLE `ogspy_game_rank_ally_honor` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_military_loose` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_military_destruct` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_military_built` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_military` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_economics` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_technology` DROP  IF EXISTS `ally`;
+ALTER TABLE `ogspy_game_rank_ally_points` DROP  IF EXISTS `ally`;
 
 -- Suppression champs player/ally classement player
 --ally
-ALTER TABLE `ogspy_game_rank_player_honor` DROP `ally`; 
-ALTER TABLE `ogspy_game_rank_player_military_loose` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_player_military_destruct` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_player_military_built` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_player_military` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_player_economics` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_player_technology` DROP `ally`;
-ALTER TABLE `ogspy_game_rank_player_points` DROP `ally`;
+ALTER TABLE `ogspy_game_rank_player_honor` DROP  IF EXISTS `ally`; 
+ALTER TABLE `ogspy_game_rank_player_military_loose` DROP IF EXISTS  `ally`;
+ALTER TABLE `ogspy_game_rank_player_military_destruct` DROP IF EXISTS  `ally`;
+ALTER TABLE `ogspy_game_rank_player_military_built` DROP IF EXISTS  `ally`;
+ALTER TABLE `ogspy_game_rank_player_military` DROP IF EXISTS  `ally`;
+ALTER TABLE `ogspy_game_rank_player_economics` DROP IF EXISTS  `ally`;
+ALTER TABLE `ogspy_game_rank_player_technology` DROP IF EXISTS  `ally`;
+ALTER TABLE `ogspy_game_rank_player_points` DROP IF EXISTS  `ally`;
 --player
-ALTER TABLE `ogspy_game_rank_player_honor` DROP `player`; 
-ALTER TABLE `ogspy_game_rank_player_military_loose` DROP `player`;
-ALTER TABLE `ogspy_game_rank_player_military_destruct` DROP `player`;
-ALTER TABLE `ogspy_game_rank_player_military_built` DROP `player`;
-ALTER TABLE `ogspy_game_rank_player_military` DROP `player`;
-ALTER TABLE `ogspy_game_rank_player_economics` DROP `player`;
-ALTER TABLE `ogspy_game_rank_player_technology` DROP `player`;
-ALTER TABLE `ogspy_game_rank_player_points` DROP `player`;
+ALTER TABLE `ogspy_game_rank_player_honor` DROP IF EXISTS  `player`; 
+ALTER TABLE `ogspy_game_rank_player_military_loose` DROP IF EXISTS  `player`;
+ALTER TABLE `ogspy_game_rank_player_military_destruct` DROP IF EXISTS  `player`;
+ALTER TABLE `ogspy_game_rank_player_military_built` DROP IF EXISTS  `player`;
+ALTER TABLE `ogspy_game_rank_player_military` DROP IF EXISTS  `player`;
+ALTER TABLE `ogspy_game_rank_player_economics` DROP IF EXISTS  `player`;
+ALTER TABLE `ogspy_game_rank_player_technology` DROP IF EXISTS  `player`;
+ALTER TABLE `ogspy_game_rank_player_points` DROP IF EXISTS  `player`;
 
 
 
